b73c50eb772288701c28fba366cc88a811d4e22e
Anthony Corletti <anthcor@gmail.com>
2014-09-28 13:35:24 -0400

    changing alot around

--- lib/ferry.rb
+++ lib/ferry.rb
@@ -1,27 +1,24 @@
-require "ferry/version"
-require "csv"
 require 'active_record'
+require 'csv'
 require 'fileutils'
+require 'ferry/version'
 require 'yaml'
-require 'ferry/tasks'
 
 module Ferry
   class Exporter
-    def to_csv()
-      # this holds all the db config information. pretty much a rosetta stone for dbs
-      info = YAML::load(IO.read("config/database.yml"))
-      # this tells us the db rails is using
-      db_type = info["production"]["adapter"]
-      case db_type
-      when "its sqlite3"
-        puts "operating with sqlite3"
-      when "mysql"
-        puts "its mysql"
-      when "postgresql"
-        puts "its postgres"
-      else
-        puts "Unknown db type or no database associated with this application."
-      end
+    # this holds all the db config information. pretty much a rosetta stone for dbs
+    info = YAML::load(IO.read("config/database.yml"))
+    # this tells us the db rails is using
+    db_type = info["production"]["adapter"]
+    case db_type
+    when "its sqlite3"
+      puts "operating with sqlite3"
+    when "postgresql"
+      puts "its postgres"
+    when "mysql"
+      puts "its mysql"
+    else
+      puts "Unknown db type or no database associated with this application."
     end
   end
 end
--- lib/ferry/tasks/export.rake
+++
@@ -1,6 +0,0 @@
-namespace :export do
-  desc "Export database to a csv file"
-  task :to_csv do
-    # code here
-  end
-end
