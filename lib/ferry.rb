ffcd3919116b9d70a9676e2d50406092e96685cd
Logan Watanabe <loganwatanabe@gmail.com>
2014-09-28 20:47:50 -0400

    now supports mysql to_csv

--- lib/ferry.rb
+++ lib/ferry.rb
@@ -24,6 +24,7 @@ module Ferry
       # puts ActiveRecord::Base.configurations[Rails.env]
 
       # type = db_type.downcase
+      # puts db_type
 
 
 
@@ -73,11 +74,62 @@ module Ferry
           end
 
 
-        when "mysql"
-          puts "its mysql"
+        when "mysql2"
+          puts "its mysql2"
+
+          info.keys.each do |environment|
+
+            if(environment == 'default')  #in Rails 4.1+ environments inherit from default, which does not have database so we will not include it
+              next
+            end
+
+
+            ActiveRecord::Base.establish_connection(    #this may not work on default rails if production db is not created (must run rake db:create:all)
+              adapter:  'mysql2',
+              host:     info[environment]['host'] || 'localhost', 
+              username: info[environment]['username'], 
+              password: info[environment]['password'], 
+              database: info[environment]['database']
+            )
+
+            unless(Dir.exists?('db/csv'))   #creating a 'csv' folder in the 'db' folder
+              Dir.mkdir('db/csv')
+              puts 'db/csv created'
+            end
+
+            unless(Dir.exists?('db/csv/'+environment))    #creating folders for each file (dev, test, prod, etc)
+              Dir.mkdir('db/csv/'+environment)
+              puts 'db/csv/'+environment+' created'
+            end
+              
+            ActiveRecord::Base.connection.tables.each do |model|                              #for each model in the db
+              columns = ActiveRecord::Base.connection.execute("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='"+info[environment]['database']+"' AND `TABLE_NAME`='"+model+"';")
+              CSV.open("db/csv/"+environment+"/"+model+".csv", "w") do |csv|    #create a csv for each table, titled 'model.csv'
+
+                col_names=[]
+                columns.each do |col|
+                  col_names.append(col[0])  #append the column names to an array, makes for good formatting
+                end
+
+                csv << col_names  #first csv row is of column names
+
+                everything = ActiveRecord::Base.connection.execute('SELECT * FROM '+model+';')    #get all the records
+
+                everything.each do |row|
+                  csv<<row
+                end
+              end
+            end
+
+          end
+
+
+
+
+
+
         when "postgresql"
-          puts "its postgres"
-          puts info.keys
+          puts "its postgresql"
 
           info.keys.each do |environment|
 
@@ -99,49 +151,37 @@ module Ferry
               puts 'db/csv created'
             end
 
-              unless(Dir.exists?('db/csv/'+environment))    #creating folders for each file (dev, test, prod, etc)
-                Dir.mkdir('db/csv/'+environment)
-                puts 'db/csv/'+environment+' created'
-              end
+            unless(Dir.exists?('db/csv/'+environment))    #creating folders for each file (dev, test, prod, etc)
+              Dir.mkdir('db/csv/'+environment)
+              puts 'db/csv/'+environment+' created'
+            end
               
-            puts ActiveRecord::Base.connection.tables
-                    ActiveRecord::Base.connection.tables.each do |model|                                #for each model in the db
-                      everything = ActiveRecord::Base.connection.execute('SELECT * FROM '+model+';')    #get all the records
+            ActiveRecord::Base.connection.tables.each do |model|                                #for each model in the db
+              everything = ActiveRecord::Base.connection.execute('SELECT * FROM '+model+';')    #get all the records
 
-                      if everything[0].nil?   #do not create a csv for an empty table
-                        next
-                      end
+              if everything[0].nil?   #do not create a csv for an empty table
+                next
+              end
 
-                      CSV.open("db/csv/"+environment+"/"+model+".csv", "w") do |csv|    #create a csv for each table, titled 'model.csv'
+              CSV.open("db/csv/"+environment+"/"+model+".csv", "w") do |csv|    #create a csv for each table, titled 'model.csv'
 
-                        size = everything[0].length / 2
-                        keys = everything[0].keys.first(size)
+                size = everything[0].length / 2
+                keys = everything[0].keys.first(size)
 
-                        csv << keys     #first row contains column names
-                        
-                        everything.each do |row|
-                          csv << row.values_at(*keys)     #subsequent rows hold record values
-                        end
-                      end
-                    end
+                csv << keys     #first row contains column names
+                
+                everything.each do |row|
+                  csv << row.values_at(*keys)     #subsequent rows hold record values
+                end
+              end
+            end
 
           end
-
-
-
-
-
-
-
-
-
 
         else
           puts "unknown db type"
       end
           
-            
-      puts Time.now()
 
     end
   end
