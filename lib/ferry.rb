8ac3001c962bb4656c8e177143ab6257d7e1e585
Logan Watanabe <loganwatanabe@gmail.com>
2014-10-05 13:31:07 -0400

    to_yaml is functional, although mysql yaml format is different from other -- ask profh about best format

--- lib/ferry.rb
+++ lib/ferry.rb
@@ -65,17 +65,17 @@ module Ferry
         end
       when "mysql2"
         puts "operating with mysql2"
-        homedir = "lib/ferry_to_csv_#{environment}"
+        homedir = "lib/ferry_to_csv_#{which_db_env}"
         puts "connected to #{which_db_env} env db"
         ActiveRecord::Base.establish_connection(adapter: db_type, database: info[which_db_env]['database'])
-        puts "connected to #{environment} env db"
+        puts "connected to #{which_db_env} env db"
         FileUtils.mkdir homedir unless Dir[homedir].present?
         puts "exporting tables to #{homedir}"
         mysql_bar = ProgressBar.new("psql_to_csv", 100)
         ActiveRecord::Base.connection.tables.each do |model|
           columns = ActiveRecord::Base.connection.execute(
             "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS`
-            WHERE `TABLE_SCHEMA`= '#{info[environment]['database']}' AND `TABLE_NAME`='#{model}';")
+            WHERE `TABLE_SCHEMA`= '#{info[which_db_env]['database']}' AND `TABLE_NAME`='#{model}';")
           CSV.open("#{homedir}/#{model}.csv", "w") do |csv|
             col_names=[]
             columns.each do |col|
@@ -116,5 +116,159 @@ module Ferry
       # profit from your new db!
 
     end
+
+# ---
+# tournaments:
+#   columns:
+#   - id
+#   - name
+#   - date
+#   - min_rank
+#   - max_rank
+#   - active
+#   records: 
+#   - - 1
+#     - 15th Annual A&M Karate Tournament
+#     - '2014-02-20'
+#     - 1
+#     - 14
+#     - true
+
+
+    def to_yaml
+      info = YAML::load(IO.read("config/database.yml"))
+      db_type = info[which_db_env]["adapter"]
+      case db_type
+      when "sqlite3"
+        puts "operating with sqlite3"
+        homedir = "lib/ferry_to_yaml_#{which_db_env}"
+        ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: info[which_db_env]['database'])
+        puts "connected to #{which_db_env} env db"
+        FileUtils.mkdir homedir unless Dir[homedir].present?
+        puts "exporting tables to #{homedir}"
+        sqlite_pbar = ProgressBar.new("sqlite_to_yaml", 100)
+        i = 0
+        ActiveRecord::Base.connection.tables.each do |model|
+          db_object = {}
+          full_table = ActiveRecord::Base.connection.execute("SELECT * FROM #{model};")
+          if !full_table[0].nil?
+            size = full_table[0].length / 2
+            keys = full_table[0].keys.first(size)
+            model_arr=[]
+            full_table.each do |row|
+              row.keep_if{|k, v| keys.include?(k)}
+              model_arr << row
+            end
+            db_object[model] = model_arr
+            if i==0
+              File.open("#{homedir}/#{which_db_env}_data.yml",'w') do |file|
+                YAML::dump(db_object, file)
+              end
+            else
+              File.open("#{homedir}/#{which_db_env}_data.yml",'a') do |file|
+                YAML::dump(db_object, file)
+              end
+            end
+            i+=1
+          end
+        end
+        sqlite_pbar.inc
+      when "postgresql"
+        puts "operating with postgres"
+        homedir = "lib/ferry_to_yaml_#{which_db_env}"
+        ActiveRecord::Base.establish_connection(adapter: db_type, database: info[which_db_env]['database'])
+        puts "connected to #{which_db_env} env db"
+        FileUtils.mkdir homedir unless Dir[homedir].present?
+        puts "exporting tables to #{homedir}"
+        psql_pbar = ProgressBar.new("psql_to_yaml", 100)
+
+        i = 0
+        ActiveRecord::Base.connection.tables.each do |model|
+          db_object = {}
+          full_table = ActiveRecord::Base.connection.execute("SELECT * FROM #{model};")
+          if full_table.num_tuples > 0
+            size = full_table[0].length / 2
+            keys = full_table[0].keys.first(size)
+            model_arr = []
+            full_table.each do |row|
+              row.keep_if{|k, v| keys.include?(k)}
+              model_arr << row
+            end
+            db_object[model] = model_arr
+            if i==0
+              File.open("#{homedir}/#{which_db_env}_data.yml",'w') do |file|
+                YAML::dump(db_object, file)
+              end
+            else
+              File.open("#{homedir}/#{which_db_env}_data.yml",'a') do |file|
+                YAML::dump(db_object, file)
+              end
+            end
+            i+=1   
+          end
+        end
+
+      when "mysql2"
+        puts "operating with mysql2"
+        homedir = "lib/ferry_to_yaml_#{which_db_env}"
+        puts "connected to #{which_db_env} env db"
+        ActiveRecord::Base.establish_connection(adapter: db_type, database: info[which_db_env]['database'])
+        puts "connected to #{which_db_env} env db"
+        FileUtils.mkdir homedir unless Dir[homedir].present?
+        puts "exporting tables to #{homedir}"
+        mysql_bar = ProgressBar.new("psql_to_yaml", 100)
+        i=0
+        ActiveRecord::Base.connection.tables.each do |model|
+          db_object = {}
+          columns = ActiveRecord::Base.connection.execute(
+            "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS`
+            WHERE `TABLE_SCHEMA`= '#{info[which_db_env]['database']}' AND `TABLE_NAME`='#{model}';")
+
+            col_names=[]
+            columns.each do |col|
+              col_names.append(col[0])
+            end
+            db_object["columns"] = col_names
+            model_arr=[]
+            full_table = ActiveRecord::Base.connection.execute("SELECT * FROM #{model};")
+            full_table.each do |row|
+              model_arr << row
+            end
+            db_object["records"] = model_arr
+            db_export = {}
+            db_export[model] = db_object
+
+            if i==0
+              File.open("#{homedir}/#{which_db_env}_data.yml",'w') do |file|
+                YAML::dump(db_export, file)
+              end
+            else
+              File.open("#{homedir}/#{which_db_env}_data.yml",'a') do |file|
+                YAML::dump(db_export, file)
+              end
+            end
+            i+=1   
+
+        end
+
+        mysql_bar.inc
+
+      when "mongo"
+        puts "mongo is currently not supported"
+      else
+        puts "Unknown db type or no database associated with this application."
+      end
+    end
+
+
+
+
+
+
+
+
+
+
+
   end
 end
