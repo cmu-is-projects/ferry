3c2cae0b6aba93e15e2ec07dfda0d69435c2a34f
Anthony Corletti <anthcor@gmail.com>
2014-10-06 12:17:05 -0400

    already yml

--- lib/ferry.rb
+++ lib/ferry.rb
@@ -103,8 +103,10 @@ module Ferry
       puts "current_db_type: #{current_db_type}"
       puts "to_new_db_type: #{switch_to_db_type}"
 
-      info[which_db_env]["adapter"] = switch_to_db_type
-      File.open("config/database.yml", "w") {|f| f.write info.to_yml}
+      if ['sqlite', 'postgresql', 'mysql'].include?(switch_to_db_type)
+        info[which_db_env]["adapter"] = switch_to_db_type
+        File.open("config/database.yml", "w") {|f| f.write info}
+      end
 
     end
   end
