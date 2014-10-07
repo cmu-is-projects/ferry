37a86af618970d5e0919d4c059c3c7b2cbb825d8
Anthony Corletti <anthcor@gmail.com>
2014-10-07 14:00:36 -0400

    db switcher works

--- lib/ferry.rb
+++ lib/ferry.rb
@@ -103,13 +103,24 @@ module Ferry
       puts "current_db_type: #{current_db_type}"
       puts "to_new_db_type: #{switch_to_db_type}"
 
-      # if ['sqlite', 'postgresql', 'mysql'].include?(switch_to_db_type)
-      #   info[which_db_env]["adapter"] = switch_to_db_type
-      #   File.open("config/database.yml", "w") {|f| f.write info}
-      # else
-      #   puts "#{switch_to_db_type} is unsupported"
-      # end
+      if ['sqlite', 'postgresql', 'mysql'].include?(switch_to_db_type)
+        info[which_db_env]["adapter"] = switch_to_db_type
+        puts "switching #{which_db_env} env to #{switch_to_db_type} ... "
+        File.open("config/database.yml", "w") {|f| f.write info.to_yaml}
+        puts "switched #{which_db_env} env to #{switch_to_db_type}"
+      else
+        puts "#{switch_to_db_type} is currently unsupported"
+      end
 
     end
+
+    def import_csv
+      # importing for different cases
+    end
+
+    def export_to_service(*args)
+      # exporting to services like AWS
+    end
+
   end
 end
