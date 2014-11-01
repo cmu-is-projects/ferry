930a29fe13a67eb9977ecedf79e65c6987b9b687
Logan Watanabe <loganwatanabe@gmail.com>
2014-11-01 13:58:27 -0400

    progressbar updated

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'progressbar', '0.21.0'
+gem 'highline'
 gemspec
--- lib/ferry/exporter.rb
+++ lib/ferry/exporter.rb
@@ -8,23 +8,26 @@ module Ferry
       homedir = "db/csv/#{environment}"
       FileUtils.mkdir homedir unless Dir[homedir].present?
       table = ActiveRecord::Base.connection.execute("SELECT * FROM #{model};")
-      table_size = table.length
-      csv_bar = ProgressBar.new("to_csv", table_size)
       CSV.open("#{homedir}/#{model}.csv", "w") do |csv|
         case db_type
           when 'sqlite3'
+            csv_bar = ProgressBar.new("to_csv", table.length)
             keys = table[0].keys.first(table[0].length / 2)
             csv << keys
             table.each do |row|
               csv << row.values_at(*keys)
+              csv_bar.inc
             end
           when 'postgresql'
+            csv_bar = ProgressBar.new("to_csv", table.num_tuples)
             keys = table[0].keys
             csv << keys
             table.each do |row|
               csv << row.values_at(*keys)
+              csv_bar.inc
             end
           when 'mysql2'
+            csv_bar = ProgressBar.new("to_csv", table.count)
             db_config = YAML::load(IO.read("config/database.yml"))
             columns = ActiveRecord::Base.connection.execute("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`= '#{db_config[environment]['database']}' AND `TABLE_NAME`='#{model}';")
             col_names=[]
@@ -34,13 +37,13 @@ module Ferry
             csv << col_names
             table.each do |row|
               csv << row
+              csv_bar.inc
             end
           else
             puts "error in db type"
             return false
           end
         end
-      csv_bar.inc(1)
       puts ""
       puts "exported to db/csv/#{environment}"
     end
@@ -51,7 +54,7 @@ module Ferry
       homedir = "db/yaml/#{environment}"
       FileUtils.mkdir homedir unless Dir[homedir].present?
       table = ActiveRecord::Base.connection.execute("SELECT * FROM #{model};")
-      table_size = table.length
+      table_size = ActiveRecord::Base.connection.execute("SELECT count(*) FROM #{model};")
       yaml_bar = ProgressBar.new("to_yaml", table_size)
         db_object = {}
         db_output = {}
@@ -62,6 +65,7 @@ module Ferry
             model_arr=[]
             table.each do |row|
               model_arr << row.values_at(*keys)
+              yaml_bar.inc
             end
             db_object["records"] = model_arr
             db_output[model] = db_object
@@ -74,6 +78,7 @@ module Ferry
             model_arr=[]
             table.each do |row|
               model_arr << row.values_at(*keys)
+              yaml_bar.inc
             end
             db_object["records"] = model_arr
             db_output[model] = db_object
@@ -91,6 +96,7 @@ module Ferry
             model_arr=[]
             table.each do |row|
               model_arr << row
+              yaml_bar.inc
             end
             db_object["records"] = model_arr
             db_output[model] = db_object
@@ -101,7 +107,6 @@ module Ferry
             puts "error in db type"
             return false
         end
-      yaml_bar.inc(1)
       puts ""
       puts "exported to db/yaml/#{environment}"
     end
