01bba246113b919426c5cc85ea6c88b4b9d3846d
Anthony Corletti <anthcor@gmail.com>
2015-04-23 19:52:04 -0400

    some work

--- doc/ferry_rake_contents.rb
+++
@@ -1,9 +0,0 @@
-# this is your ferry initialzer file for any rake tasks you would like to create
-# in this file you can write rake tasks that are easily tailored to more case-by-case user implementations
-# but can still include functionality provided by ferry
-# more configurable env options coming soon via Ferry's DSL that will be written soon!
-# so this will be very obsolete soon!
-
-namespace :ferry do
-  # your code here!
-end
---
+++ doc/ferryfile_contents.rb
@@ -0,0 +1,1 @@
+load "config/captain"
